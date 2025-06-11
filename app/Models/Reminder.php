86a703554cdba85b48666947671578feb6d23c59
nathanzagrovic <138458652+nathanzagrovic@users.noreply.github.com>
2025-06-11 20:51:10 +0100

    Add basic toggle + UI

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +11,12 @@ class Reminder extends Model
 {
     /** @use HasFactory<\Database\Factories\ReminderFactory> */
     use HasFactory;
+
+    protected function completedAt(): Attribute
+    {
+        return Attribute::get(fn ($value) =>
+        $value ? Carbon::parse($value)->format('d/m/y') : null
+        );
+    }
+
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 import { usePage } from '@inertiajs/vue3';
 import { Link } from '@inertiajs/vue3';
 const reminders = usePage().props.reminders;
+// TODO: Extract reminder wrapper + reminder item to components
 </script>
 
 <template>
@@ -17,28 +18,25 @@ const reminders = usePage().props.reminders;
             </h2>
         </template>
 
+
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
+                <h2 class="text-2xl font-bold mb-2">📋 Reminders</h2>
+                <div class="grid grid-cols-2">
+                    <div class="overflow-hidden bg-white p-6 border border-gray- shadow-sm sm:rounded-lg">
+                    <ul class="space-y-2" v-if="reminders">
+                        <li v-for="reminder in reminders" :key="reminder.id" class="group cursor-pointer">
+                            <Link class="flex items-center gap-2" :href="`/toggle-complete/${reminder.id}`" method="post" as="button" :preserve-state="false">
+                                <div :class="!reminder.completed_at ? 'border' : ''" class="w-5 h-5 rounded border-gray-300 bg-white flex items-center text-xs justify-center">
+                                    <span class="text-lg" v-if="reminder.completed_at">✅</span>
+                                </div>
+                                <span :class="reminder.completed_at ? 'line-through opacity-25' : ''" class="group-hover:underline" v-text="reminder.title"></span>
+                                <span v-if="reminder.completed_at" class="text-xs">- Completed on {{ reminder.completed_at }}</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-        </div>
-
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <ul v-if="reminders">
-                    <li v-for="reminder in reminders" :key="reminder.id">
-                        <Link :href="`/toggle-complete/${reminder.id}`" method="post" as="button" :preserve-state="false">X</Link>
-                        <span v-text="reminder.title"></span>
-                        <span v-text="reminder.completed_at"></span>
-                    </li>
-                </ul>
+                </div>
             </div>
         </div>
 
